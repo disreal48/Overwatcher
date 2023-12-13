12ecd93b873037ec840216776b78d8e3a31c4222
disreal48 <disreal48@googlemail.com>
2023-12-13 23:26:50 +0100

    added pokemonList Array

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
-alert('Hell World!');
-
-let favoriteFood = 'Pizza';
-
-document.write(favoriteFood);
+let pokemonList = [
+    { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+    { name: "Charmander", height: 0.6, types: ["fire"] },
+    { name: "Squirtle", height: 0.5, types: ["water"] },
+    { name: "Pikachu", height: 0.4, types: ["electric"] },
+    { name: "Eevee", height: 0.3, types: ["normal"] }
+];
