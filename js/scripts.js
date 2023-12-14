f90d0d9e255c2d1e1cbf0cbcaff7a6776d72be2c
disreal48 <disreal48@googlemail.com>
2023-12-14 12:38:06 +0100

    added a printing function and some styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,36 @@
-let pokemonList = [
-    { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
-    { name: "Charmander", height: 0.6, types: ["fire"] },
-    { name: "Squirtle", height: 0.5, types: ["water"] },
-    { name: "Pikachu", height: 0.4, types: ["electric"] },
-    { name: "Eevee", height: 0.3, types: ["normal"] }
+const pokemonList = [
+  { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+  { name: "Charmander", height: 0.6, types: ["fire"] },
+  { name: "Squirtle", height: 0.5, types: ["water"] },
+  { name: "Pikachu", height: 0.4, types: ["electric"] },
+  { name: "Eevee", height: 0.3, types: ["normal"] },
 ];
+
+let cards = document.getElementById("cards");
+
+// This is a for loop that iterates over each item in pokemonList.
+// It writes the name and height of each pokemon to the browser window.
+// For pokemon greater than or equal to 0.7 in height, it adds the text "Wow, that's big!".
+
+const pokemonPrinter = (pokemonList) => {
+  for (let i = 0; i < pokemonList.length; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("card");
+    let cardContent = "";
+
+    if (pokemonList[i].height >= 0.7) {
+      cardContent = document.createTextNode(
+        `${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big!`
+      );
+    } else {
+      cardContent = document.createTextNode(
+        `${pokemonList[i].name} (height: ${pokemonList[i].height})`
+      );
+    }
+
+    newDiv.appendChild(cardContent);
+    document.getElementById("cards").appendChild(newDiv);
+  }
+};
+
+pokemonPrinter(pokemonList);
